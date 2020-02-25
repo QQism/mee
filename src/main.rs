2c2d2e9eb3690fd56a906bbd9c9993903ab20b2c
QQ <me@quang.be>
2020-02-26 00:35:01 +1100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,28 @@ use crossterm::{
     cursor,
     event::{read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent, KeyModifiers},
     execute,
-    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
+    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType, size, ScrollUp, ScrollDown},
     QueueableCommand,
     queue,
     Result,
 };
 
+struct TerminalSize {
+    cols: u16,
+    rows: u16,
+}
+
 fn match_event() -> Result<()> {
     let mut stdout = stdout();
     let mut line = String::new();
     let mut current_max_column = 0;
+
+    let (terminal_cols, terminal_rows) = size()?;
+
+    let mut terminal_size = TerminalSize {
+        cols: terminal_cols,
+        rows: terminal_rows,
+    };
 
     loop {
         let event = read()?;
@@ -30,7 +42,7 @@ fn match_event() -> Result<()> {
             }) => {
                 // cursor::MoveToNextLine(1);
                 // Print the suggestion move to the next line
-                echo(&mut stdout, line.clone());
+                echo(&mut stdout, &mut terminal_size, line.clone())?;
                 line.clear();
             }
             Event::Key(KeyEvent { code: KeyCode::Left, .. }) => { stdout.queue(cursor::MoveLeft(1)).expect("Error"); }
@@ -58,7 +70,7 @@ fn match_event() -> Result<()> {
 
                 current_max_column -= 1;
 
-                show_sugesstions(&mut stdout, line.clone());
+                show_suggestions(&mut stdout, &mut terminal_size, line.clone())?;
             }
             Event::Key(KeyEvent {
                 code: KeyCode::Char(c), ..
@@ -92,7 +104,12 @@ fn match_event() -> Result<()> {
 
                 current_max_column += 1;
 
-                show_sugesstions(&mut stdout, line.clone());
+                show_suggestions(&mut stdout, &mut terminal_size, line.clone())?;
+            }
+            Event::Resize(columns, rows) => {
+                println!("Terminal size changed: Columns {} Rows {}", columns, rows);
+                terminal_size.cols = columns;
+                terminal_size.rows = rows;
             }
             _ => {}
         }
@@ -103,7 +120,8 @@ fn match_event() -> Result<()> {
     Ok(())
 }
 
-fn clear_suggestions(stdout: &mut Stdout) -> Result<()> {
+fn clear_suggestions(stdout: &mut Stdout, terminal_size: &mut TerminalSize) -> Result<()> {
+    let (col, _) = cursor::position().unwrap();
     queue!(stdout,
            cursor::SavePosition,
            cursor::MoveToNextLine(1),
@@ -117,8 +135,18 @@ fn clear_suggestions(stdout: &mut Stdout) -> Result<()> {
     Ok(())
 }
 
-fn show_sugesstions(mut stdout: &mut Stdout, line: String) -> Result<()> {
-    clear_suggestions(&mut stdout)?;
+fn show_suggestions(mut stdout: &mut Stdout, terminal_size: &mut TerminalSize, line: String) -> Result<()> {
+    clear_suggestions(&mut stdout, terminal_size)?;
+
+    // let (_, rows) = cursor::position().unwrap();
+
+    // if (rows + 3) >= terminal_size.rows {
+    //     queue!(stdout,
+    //            cursor::SavePosition,
+    //            ScrollUp(3),
+    //            cursor::RestorePosition)?;
+    // }
+
     queue!(stdout, cursor::SavePosition, cursor::MoveToNextLine(1))?;
     write!(stdout, "\u{250C}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}")?;
     queue!(stdout, cursor::MoveToNextLine(1))?;
@@ -130,12 +158,12 @@ fn show_sugesstions(mut stdout: &mut Stdout, line: String) -> Result<()> {
     Ok(())
 }
 
-fn echo(mut stdout: &mut Stdout, line: String) -> Result<()> {
-    clear_suggestions(&mut stdout)?;
+fn echo(mut stdout: &mut Stdout, terminal_size: &mut TerminalSize, line: String) -> Result<()> {
+    clear_suggestions(&mut stdout, terminal_size)?;
 
     queue!(stdout, cursor::MoveToNextLine(1))?;
 
-    print!("You typed: {}", line);
+    write!(stdout, "You typed: {}", line)?;
 
     queue!(stdout, cursor::MoveToNextLine(1))?;
 
